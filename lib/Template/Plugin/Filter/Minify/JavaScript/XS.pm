c48f6eb30a7c57fd92fef79ff5788099580411b3
Michael Schout <mschout@gkg.net>
2010-03-31 08:15:20 -0500

    use Dist::Zilla

--- lib/Template/Plugin/Filter/Minify/JavaScript/XS.pm
+++ lib/Template/Plugin/Filter/Minify/JavaScript/XS.pm
@@ -1,11 +1,11 @@
 package Template::Plugin::Filter::Minify::JavaScript::XS;
 
+# ABSTRACT: JavaScript::Minifier::XS filter for Template Toolkit
+
 use 5.006;
 use strict;
 use base 'Template::Plugin::Filter';
 use JavaScript::Minifier::XS;
-
-our $VERSION = '0.92';
 
 sub init {
     my $self = shift;
@@ -25,9 +25,14 @@ sub filter {
 
 1;
 
-=head1 NAME
+__END__
 
-Template::Plugin::Filter::Minify::JavaScript::XS - JavaScript::Minifier::XS filter for Template Toolkit
+=begin Pod::Coverage
+
+init
+filter
+
+=end Pod::Coverage
 
 =head1 SYNOPSIS
 
@@ -45,44 +50,6 @@ Template::Plugin::Filter::Minify::JavaScript::XS - JavaScript::Minifier::XS filt
 
 This module is a Template Toolkit filter, which uses JavaScript::Minifier::XS
 to compress javascript code from filtered content during template processing.
-
-=head1 SOURCE
-
-You can contribute or fork this project via github:
-
-http://github.com/mschout/template-plugin-filter-minify-javascript-xs
-
- git clone git://github.com/mschout/template-plugin-filter-minify-javascript-xs.git
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-bug-template-plugin-filter-minify-javascript-xs@rt.cpan.org, or through the web
-interface at http://rt.cpan.org/
-
-=head1 AUTHOR
-
-Michael Schout E<lt>mschout@cpan.orgE<gt>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Michael Schout.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of either:
-
-=over 4
-
-=item *
-
-the GNU General Public License as published by the Free Software Foundation;
-either version 1, or (at your option) any later version, or
-
-=item *
-
-the Artistic License version 2.0.
-
-=back
 
 =head1 SEE ALSO
 
